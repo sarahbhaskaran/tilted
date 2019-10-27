8bcd318b7466ec62c8ff4c4f9688d0e5608ea45a
Sarah <sbhaskaran1357@berkeley.edu>
2019-10-26 18:16:31 -0700

    pdf_reader cleaned up and working

--- pdf_reader.py
+++ pdf_reader.py
@@ -3,62 +3,45 @@ from PIL import Image, ImageTk
 import time
 import tkinter as tk
 
-class PDFpage():
+class PDF():
     page_index = 0
     pages = 0
-    currpage = None
+    canvas = None
+    root = None
+    page_file = 'pg.png'
     def __init__(self, filename):
         self.file = filename
         self.doc = fitz.open(filename)
         self.pages = self.doc.pageCount
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root)
+        self.canvas.pack()
+        self.showPage()
 
     def turnPage(self):
-        self.page_index += 1
+        self.page_index = min(self.page_index + 1, self.pages - 1)
         self.showPage()
-        self.currpage.close()
 
     def showPage(self):
         page = self.doc.loadPage(self.page_index)
         pix = page.getPixmap()
+        pix.writePNG("pg.png")
 
-        mode = "RGBA" if pix.alpha else "RGB"
-        img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
-        self.currpage = img
+        self.canvas.config(height=pix.height, width=pix.width)
+        open_image = Image.open(self.page_file)
+        img = ImageTk.PhotoImage(open_image)
 
-        img.show()
-        time.sleep(2)
-        img.close()
+        self.canvas.create_image(20,20, anchor=tk.NW, image=img)
+        self.root.update_idletasks()
+        self.root.update()
 
-doc = fitz.open("test.pdf")
-pages = doc.pageCount
+    def closeWindow(self):
+        self.root.destroy()
 
-root = tk.Tk()
-canvas = tk.Canvas(root, width = 300, height = 300)
-canvas.pack()
 
-for i in range(pages):
-    page = doc.loadPage(i)
-    pix = page.getPixmap()
-    pix.writePNG("pg.png")
-    #
-    # mode = "RGBA" if pix.alpha else "RGB"
-    # img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
-
-    # img.show()
-    open_image = Image.open("pg.PNG")
-    img = ImageTk.PhotoImage(open_image)
-
-    canvas.create_image(20,20, anchor=tk.NW, image=img)
-    root.update_idletasks()
+pdf = PDF('test.pdf')
+for i in range(4):
     time.sleep(2)
-    # tk.mainloop()
-
-    # img = Image.open('pg.png')
-    # img.show()
-    # time.sleep(2)
-    # img.load()
-    # img.close()
-# pdf = PDFpage('test.pdf')
-# pdf.showPage()
-# time.sleep(2)
-# pdf.turnPage()
+    pdf.turnPage()
+time.sleep(1)
+pdf.closeWindow()
