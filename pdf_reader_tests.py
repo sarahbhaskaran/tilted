0ce81bab8dce940615c6b35f43e4308710a74246
Sarah <sbhaskaran1357@berkeley.edu>
2019-10-26 20:22:55 -0700

    beginning desktop gui

--- pdf_reader_tests.py
+++ pdf_reader_tests.py
@@ -1,21 +1,25 @@
 import time
 from pdf_reader import PDFReader
 
-pdf = PDFReader('test.pdf')
-for i in range(4):
-    time.sleep(2)
-    pdf.turnForward()
-for i in range(3):
+def main(filename = 'test.pdf'):
+    pdf = PDFReader(filename)
+    for i in range(4):
+        time.sleep(2)
+        pdf.turnForward()
+    for i in range(3):
+        time.sleep(1)
+        pdf.turnBackward()
     time.sleep(1)
-    pdf.turnBackward()
-time.sleep(1)
-pdf.goToPage(2)
-time.sleep(1)
-pdf.goToPage(0)
-time.sleep(1)
-# Should go to page 3 (fourth page)
-pdf.goToPage(5)
-time.sleep(1)
-# Should go to page 0
-pdf.goToPage(-1)
-pdf.closeWindow()
+    pdf.goToPage(2)
+    time.sleep(1)
+    pdf.goToPage(0)
+    time.sleep(1)
+    # Should go to page 3 (fourth page)
+    pdf.goToPage(5)
+    time.sleep(1)
+    # Should go to page 0
+    pdf.goToPage(-1)
+    pdf.closeWindow()
+
+if __name__=="__main__":
+    main()
