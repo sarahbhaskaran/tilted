68887b8eb01dfdaca8395ecb78a0e6b6e725a19e
Sarah Bhaskaran <sbhaskaran1357@berkeley.edu>
2020-06-25 12:44:45 -0700

    Not useful changes; will override

--- jstilted/pdfdisplay.js
+++ jstilted/pdfdisplay.js
@@ -2,7 +2,13 @@
 class pdfdisplay {
     showPage() {
         console.log("showpage function");
-        const pdf_doc = pdfjsLib.getDocument("C:\Users\Sarah\tilted\jstilted\web");
+        // const filename = "C:/Users/Sarah/tilted/jstilted/input/test.pdf"
+        const filename = "../input/test.pdf"
+        var file = new File(124393, filename);
+        var fileReader = new FileReader();  
+        var typedarray = new Uint8Array(filename);
+        console.log(typedarray);
+        const pdf_doc = pdfjsLib.getDocument(typedarray);
         console.log(pdf_doc.numPages);
     }
 }
