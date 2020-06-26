d6be9def879b4b469933415a941412e35bfba90d
Sarah Bhaskaran <sbhaskaran1357@berkeley.edu>
2020-06-26 13:07:26 -0700

    Tried to put pdf rendering in pdfdisplay

--- jstilted/pdfdisplay.js
+++ jstilted/pdfdisplay.js
@@ -1,14 +1,60 @@
 
 class pdfdisplay {
-    showPage() {
-        console.log("showpage function");
-        // const filename = "C:/Users/Sarah/tilted/jstilted/input/test.pdf"
-        const filename = "../input/test.pdf"
-        var file = new File(124393, filename);
-        var fileReader = new FileReader();  
-        var typedarray = new Uint8Array(filename);
-        console.log(typedarray);
-        const pdf_doc = pdfjsLib.getDocument(typedarray);
-        console.log(pdf_doc.numPages);
+    fileReader;
+    totalPages;
+    currPage;
+    file;
+
+    constructor(file) {
+        this.currPage = 1;
+        this.file = file;
+        this.loadFile(file);
+        this.totalPage = this.fileReader.numPages;
     }
+
+    loadFile() {
+        this.fileReader = new FileReader();
+        console.log("after creation");
+        console.log(this.fileReader);
+        // this.fileReader.readAsArrayBuffer(this.file);
+        this.fileReader.onload = this.render;
+    }
+
+    render() {
+        console.log('render function');
+        console.log(this.fileReader);
+        console.log(this.file);
+        var typedarray = new Uint8Array(this.fileReader.result);
+        console.log(typedarray);
+        const loadingTask = pdfjsLib.getDocument(typedarray);
+        loadingTask.promise.then(pdf => {
+        // The document is loaded here...
+            pdf.getPage(this.currPage).then(function(page) {
+            console.log('Page loaded');
+
+            var scale = .7;
+            var viewport = page.getViewport({
+            scale: scale
+            });
+
+            var canvas = document.getElementById('pdfCanvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+            canvasContext: context,
+            viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(function() {
+            console.log('Page rendered');
+            });
+
+        });
+        //end of example code
+        });
+    }
+
 }
